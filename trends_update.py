e18c0722dd22501bc34047ae4ab353d87584cce6
arifarfx <arifarforex@gmail.com>
2024-12-21 03:21:41 +0700

    Update trends_update.py

--- trends_update.py
+++ trends_update.py
@@ -16,12 +16,20 @@ countries = [
 # Fungsi untuk mengambil data dari file .txt
 def get_keywords(country):
     url = f"https://raw.githubusercontent.com/arifarfx/Google-Trends-Keywords-Scraper/refs/heads/main/forcopied/{country}.txt"
-    response = requests.get(url)
-    response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
-    data = response.text.splitlines()
-    update_time = data[1].replace("Update Data ", "")
-    keywords = data[2]
-    return keywords, update_time
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+        data = response.text.splitlines()
+        if len(data) >= 3:  # Pastikan file memiliki minimal 3 baris
+            update_time = data[1].replace("Update Data ", "")
+            keywords = data[2]
+            return keywords, update_time
+        else:
+            print(f"Error: File {country}.txt tidak valid")
+            return "", ""  # Kembalikan string kosong jika file tidak valid
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return "", ""  # Kembalikan string kosong jika ada error
 
 # Buat tabel Markdown
 table_data = []
